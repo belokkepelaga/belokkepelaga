38d554af471042d7bd74b84fb70b8cc69148eba9
haydaramru <haydaramrurevanda@mail.ugm.ac.id>
2025-08-04 21:47:31 +0800

    feat: update Hero component to use new webp image and adjust layout for title positioning

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
     return (
         <AnimatedContainer className={'overflow-hidden'}>
             <div className="h-[52rem] absolute left-0 w-full overflow-hidden">
-                <Image fill sizes="auto" className="object-cover min-w-96" src="/tukad-bangkung.png" alt="Travel Hero Background Image" />
+                <Image fill sizes="auto" className="object-cover min-w-96" src="/tukad-bangkung.webp" alt="Jembatan Tukad Bangkung"/>
                 <div className="absolute inset-0 z-1 opacity-75 bg-[linear-gradient(180deg,rgba(0,0,0,0.50)_49.65%,rgba(0,0,0,0.00)_100%)]"></div>
-                {/* <div className="absolute lg:opacity-100 opacity-50 z-10 bottom-0 inset-x-0 h-[16rem] bg-[linear-gradient(180deg,rgba(255,255,255,0.00)_0%,#FFF_62.59%,#FFF_100%)] dark:bg-[linear-gradient(180deg,rgba(9,9,11,0.00)_0%,rgba(9,9,11,0.8)_62.59%,rgba(9,9,11,1)_100%)] lg:backdrop-blur-[0.75px]"></div> */}
+                {/* <div className="absolute lg:opacity-100 opacity-50 z-10 bottom-0 inset-x-0 h-[16rem] bg-[linear-gradient(180deg,rgba(255,255,255,0.00)_0%,#FFF_62.59%,#FFF_100%)] dark:bg-[linear-gradient(180deg,rgba(9,9,11,0.00)_0%,rgba(9,9,11,0.8)_62.59%,rgba(9,9,11,1)_100%)] lg:backdrop-blur-[0.5px]"></div> */}
             </div>
             <div className="container relative z-10">
                 <div className="relative rounded-b-md rounded-t-3xl lg:rounded-t-4xl min-h-[52rem]">
@@ -49,12 +49,12 @@ const Hero = () => {
                             src="/cloud-5.png"
                             alt="Travel Hero Cloud Image"
                         /> */}
-                        <div className="absolute left-1/2 -translate-x-1/2 top-24 z-3 flex flex-col items-center gap-y-4">
+                        <div className="absolute left-1/2 -translate-x-1/2 top-48 z-3 flex flex-col items-center gap-y-4">
+                            <div className="title  text-lg lg:text-5xl bg-[linear-gradient(180deg,rgba(255,255,255,0.80)_4.92%,rgba(255,255,255,0.40)_89.39%)] leading-none">KKN-PPM UGM 2025</div>
                             <div className="title bg-[linear-gradient(180deg,#FFF_-16.99%,rgba(255,255,255,0.00)_100%)] text-3xl lg:text-8xl leading-none whitespace-nowrap">
                                 <span className="font-pinyon text-8xl lg:text-[18rem] !font-normal">B</span><span className="text-5xl lg:text-[9rem]">elok ke </span>
                                 <span className="font-pinyon text-8xl lg:text-[18rem] !font-normal">P</span><span className="text-5xl lg:text-[9rem]">elaga</span>
                             </div>
-                            {/* <div className="title  text-lg lg:text-5xl bg-[linear-gradient(180deg,rgba(255,255,255,0.80)_4.92%,rgba(255,255,255,0.40)_89.39%)] leading-none">KKN-PPM UGM 2025</div> */}
                         </div>
                     </div>
                 </div>
